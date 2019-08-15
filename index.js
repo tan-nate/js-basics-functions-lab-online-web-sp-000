68efe2e8d10f5e6a81ef38520ec3b4b4c39834a8
Nate Tan <nktan93@gmail.com>
2019-08-15 18:23:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function distanceFromHqInBlocks(originBlock) {
   return Math.abs(originBlock - 42);
 }
+
+function distanceFromHqInFeet(originBlock) {
+  return distanceFromHqInBlocks(originBlock) * 264;
+}
