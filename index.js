47d7b0191762ac05866e64dae3cc00a433e7534e
Nate Tan <nktan93@gmail.com>
2019-08-15 18:42:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ function distanceTravelledInFeet(startingBlock, endingBlock) {
 function calculatesFarePrice(start, destination) {
   if (distanceTravelledInFeet(start, destination) <= 400) {
     return 0;
-  } else if (distanceTravelledInFeet(start, destination) > 400 && (distanceTravelledInFeet(start, destination) <= 2000) {
+  } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
     return 0.02 * (distanceTravelledInFeet(start, destination) - 400);
-  } else if (distanceTravelledInFeet(start, destination) > 2000 && (distanceTravelledInFeet(start, destination) <= 2500) {
+  } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
     return 25;
   } else {
     return "cannot travel that far";
   }
-} 
+}
